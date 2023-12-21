41682f397a36d969584a1c0fc2dfd681b1df0316
MohdShoaib530 <mohdshoaib91530@gmail.com>
2023-12-21 23:43:06 +0530

    adding functionality

--- tic-tac-toe/src/Components/Grid/Grid.jsx
+++ tic-tac-toe/src/Components/Grid/Grid.jsx
@@ -4,10 +4,14 @@ import './Grid.css'
 
 function Grid({numberOfCards}){
     const [board, setBoard] = useState(Array(numberOfCards).fill(""));
+    const  [turn,setTurn] = useState(true)
 
     return (
-        <div className="grid">
-            {board.map((el,ind) => <Card key={ind}/>)}
+        <div className="grid-wrapper">
+            <h1 className="grid-highlight">Current turn:{(turn) ? 'O' : 'X'}</h1>
+            <div className="grid">
+               {board.map((el,ind) => <Card key={ind}/>)}
+            </div>
         </div>
     )
 };
