a3c416d30629a057d718e85029aa67aa220b466c
MohdShoaib530 <mohdshoaib91530@gmail.com>
2023-12-31 23:33:03 +0530

    created a usePokemonList custom hook

--- pokedex/src/hooks/usePokemonList.js
+++ pokedex/src/hooks/usePokemonList.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-function usePokemonList(){
+function usePokemonList(url,type){
     const [pokemonListState, setPokemonListState] = useState({
         pokemonList: [],
         isLoading: true,
@@ -15,34 +15,44 @@ function usePokemonList(){
       
         try {
           const response = await axios.get(pokemonListState.pokedexUrl);
+          console.log('response',response.data.pokemon);
           setPokemonListState((prevState) => ({
             ...prevState,
             nextUrl: response.data.next,
             prevUrl: response.data.previous,
           }));
+
+          if(type){
+            setPokemonListState((prevState) => ({
+                ...prevState,
+                pokemonList: response.data.pokemon.slice(0, 5),
+              }));
+          } else {
+
+              const pokemonResult = response.data.results;
+              const pokemonResultPromise = pokemonResult.map((pokemon) =>
+                axios.get(pokemon.url)
+              );
+          
+              const pokemonData = await axios.all(pokemonResultPromise);
+          
+              const result = pokemonData.map((pokeData) => {
+                const pokemon = pokeData.data;
+                return {
+                  id: pokemon.id,
+                  name: pokemon.name,
+                  image: pokemon.sprites.other.dream_world.front_default,
+                  types: pokemon.types,
+                };
+              });
+          
+              setPokemonListState((prevState) => ({
+                ...prevState,
+                pokemonList: result,
+                isLoading: false,
+              }));
+          }
       
-          const pokemonResult = response.data.results;
-          const pokemonResultPromise = pokemonResult.map((pokemon) =>
-            axios.get(pokemon.url)
-          );
-      
-          const pokemonData = await axios.all(pokemonResultPromise);
-      
-          const result = pokemonData.map((pokeData) => {
-            const pokemon = pokeData.data;
-            return {
-              id: pokemon.id,
-              name: pokemon.name,
-              image: pokemon.sprites.other.dream_world.front_default,
-              types: pokemon.types,
-            };
-          });
-      
-          setPokemonListState((prevState) => ({
-            ...prevState,
-            pokemonList: result,
-            isLoading: false,
-          }));
         } catch (error) {
           console.error('Error fetching data:', error);
           setPokemonListState((prevState) => ({ ...prevState, isLoading: false }));
