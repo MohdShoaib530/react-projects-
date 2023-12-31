9e623378dd37b293e79b289f665fecba36c7bc2c
MohdShoaib530 <mohdshoaib91530@gmail.com>
2023-12-31 17:11:09 +0530

    created custom hook

--- pokedex/src/components/PokemonList/PokemonList.jsx
+++ pokedex/src/components/PokemonList/PokemonList.jsx
@@ -1,68 +1,11 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import "./PokemonList.css";
 import Pokemon from "../Pokemon/Pokemon";
+import usePokemonList from "../../hooks/usePokemonList";
+
 
 function PokemonList() {
-  // const [pokemonList,setPokemonList] = useState([]);
-  // const [isLoading,setIsLoading] = useState(true)
-
-  // const [pokedexUrl,setUrl] = useState("https://pokeapi.co/api/v2/pokemon")
-
-  // const [nextUrl,setNextUrl] = useState('')
-  // const [prevUrl,setPrevUrl] = useState('')
-
-  const [pokemonListState, setPokemonListState] = useState({
-    pokemonList: [],
-    isLoading: true,
-    pokedexUrl: "https://pokeapi.co/api/v2/pokemon",
-    nextUrl: "",
-    prevUrl: "",
-  });
-  async function fetchData() {
-    setPokemonListState((prevState) => ({ ...prevState, isLoading: true }));
-  
-    try {
-      const response = await axios.get(pokemonListState.pokedexUrl);
-      setPokemonListState((prevState) => ({
-        ...prevState,
-        nextUrl: response.data.next,
-        prevUrl: response.data.previous,
-      }));
-  
-      const pokemonResult = response.data.results;
-      const pokemonResultPromise = pokemonResult.map((pokemon) =>
-        axios.get(pokemon.url)
-      );
-  
-      const pokemonData = await axios.all(pokemonResultPromise);
-  
-      const result = pokemonData.map((pokeData) => {
-        const pokemon = pokeData.data;
-        return {
-          id: pokemon.id,
-          name: pokemon.name,
-          image: pokemon.sprites.other.dream_world.front_default,
-          types: pokemon.types,
-        };
-      });
-  
-      setPokemonListState((prevState) => ({
-        ...prevState,
-        pokemonList: result,
-        isLoading: false,
-      }));
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setPokemonListState((prevState) => ({ ...prevState, isLoading: false }));
-    }
-  }
-  
-  useEffect(() => {
-    fetchData();
-  }, [pokemonListState.pokedexUrl]);
-
-
+   
+  const [pokemonListState, setPokemonListState] = usePokemonList()
   return (
     <div className="pokemon-list-wrapper">
       <div className="pokemon-wrapper">
---
+++ pokedex/src/hooks/usePokemonList.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function usePokemonList(){
+    const [pokemonListState, setPokemonListState] = useState({
+        pokemonList: [],
+        isLoading: true,
+        pokedexUrl: "https://pokeapi.co/api/v2/pokemon",
+        nextUrl: "",
+        prevUrl: "",
+      });
+    
+      async function fetchData() {
+        setPokemonListState((prevState) => ({ ...prevState, isLoading: true }));
+      
+        try {
+          const response = await axios.get(pokemonListState.pokedexUrl);
+          setPokemonListState((prevState) => ({
+            ...prevState,
+            nextUrl: response.data.next,
+            prevUrl: response.data.previous,
+          }));
+      
+          const pokemonResult = response.data.results;
+          const pokemonResultPromise = pokemonResult.map((pokemon) =>
+            axios.get(pokemon.url)
+          );
+      
+          const pokemonData = await axios.all(pokemonResultPromise);
+      
+          const result = pokemonData.map((pokeData) => {
+            const pokemon = pokeData.data;
+            return {
+              id: pokemon.id,
+              name: pokemon.name,
+              image: pokemon.sprites.other.dream_world.front_default,
+              types: pokemon.types,
+            };
+          });
+      
+          setPokemonListState((prevState) => ({
+            ...prevState,
+            pokemonList: result,
+            isLoading: false,
+          }));
+        } catch (error) {
+          console.error('Error fetching data:', error);
+          setPokemonListState((prevState) => ({ ...prevState, isLoading: false }));
+        }
+      };
+    
+      useEffect(() => {
+        fetchData();
+      }, [pokemonListState.pokedexUrl]);
+
+        return [pokemonListState, setPokemonListState];
+}
+
+export default usePokemonList;
